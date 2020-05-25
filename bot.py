350d8fa45c83b43edc4cf2de5ccba1ac974bc344
ivan kov <63105949+IKolyas@users.noreply.github.com>
2020-05-25 08:55:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -153,7 +153,7 @@ class ScheduleMessage:
         p1.start()
 
 
-schedule.every().day.at("03:40").do(TimerUs().timer_news)
+schedule.every().day.at("03:30").do(TimerUs().timer_news)
 schedule.every().day.at("12:50").do(TimerUs().timer_news)
 
 if __name__ == '__main__':
