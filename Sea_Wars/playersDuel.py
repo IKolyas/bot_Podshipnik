065eac4630632b0feaebbe781b81778ac5fa2b60
ivan kov <63105949+IKolyas@users.noreply.github.com>
2020-08-03 14:35:51 +0500

    com

---
+++ Sea_Wars/playersDuel.py
@@ -0,0 +1,12 @@
+from createArena import Arena
+
+
+class Duel:
+    playerName1 = None
+    playerName2 = None
+
+    logs = None
+
+    def __init__(self):
+        self.playerName1 = Arena()
+        self.playerName2 = Arena()
